d411a580619a7405674afc2f5e3f7d094d2db5c2
Samuel Austin <samaust@hotmail.com>
2025-03-31 20:00:54 -0400

    Fix setup.py
    update path to load __version__

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 here = pathlib.Path(__file__).parent
 __version__ = re.search(
     r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
-    io.open(here / 'pygltflib' / '__init__.py', encoding='utf_8_sig').read()
+    io.open(here / 'pygltflib' / 'v2' / 'schema.py', encoding='utf_8_sig').read()
     ).group(1)
 
 
